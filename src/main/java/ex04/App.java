b0262f2adf4bcb5dd2387e8dd6615a912fe1b58c
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-06-04 19:05:06 -0400

    Initial commit

---
+++ src/main/java/ex04/App.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Evan Raiford
+ */
+package ex04;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a noun: ");
+        String noun = in.nextLine();
+        System.out.print("Enter a verb. ");
+        String verb = in.nextLine();
+        System.out.print("Enter an adjective. ");
+        String adjective = in.nextLine();
+        System.out.print("Enter an adverb. ");
+        String adverb = in.nextLine();
+        String fullString = "Do you " + verb + " your " + adjective + " " + noun + " " + adverb +"? That's hilarious!";
+        System.out.println(fullString);
+    }
+}
